df5466473ba7392c7ff637645a3f83c5f59054b7
sagar <sagar.y@simformsolutions.com>
2022-09-26 18:46:47 +0530

    Html report Added

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '73r3pj',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: "https://opensource-demo.orangehrmlive.com",
-    "watchForFileChanges": false
+    "watchForFileChanges": false,
+   setupNodeEvents(on, config) {
+   require('cypress-mochawesome-reporter/plugin')(on);
     
-    
-    
+   }
 
    // setupNodeEvents(on, config) {
       // implement node event listeners here
